4da8047f787ed802c45fa0d04d7f641ec7a3d6a9
Artiom Kolesnikov <artiom.kolesnikov@gmail.com>
2017-08-07 11:42:36 -0300

    Cambio dirección del servidor API

--- config.production.js
+++ config.production.js
@@ -2,8 +2,10 @@
  * Created by kolesnikov-a on 31/07/2017.
  */
 consultaTurnosApp.constant('APP_CONFIG', {
-	SERVER_URL: "http://terminales.puertobuenosaires.gob.ar:8090",
-	SOCKET_URL: "http://terminales.puertobuenosaires.gob.ar:8090"
+	//SERVER_URL: "http://terminales.puertobuenosaires.gob.ar:8090",
+	//SOCKET_URL: "http://terminales.puertobuenosaires.gob.ar:8090"
+	SERVER_URL: "http://190.221.146.245:8090",
+	SOCKET_URL: "http://190.221.146.245:8090"
 	//SERVER_URL: "http://10.1.0.61:8090",
 	//SOCKET_URL: "http://10.1.0.61:8090"
 });
