202d12a0c5cdc9108d7b8aff346ecf52a693c41b
Artiom Kolesnikov <artiom.kolesnikov@gmail.com>
2017-08-25 15:34:26 -0300

    Agrego directiva faltanta para convertir texto a mayúsculas

--- consulta/consulta.controller.js
+++ consulta/consulta.controller.js
@@ -45,3 +45,23 @@ consultaTurnosApp.controller('turnosConsultaCtrl', ['$scope', 'turnosFactory', '
 		})
 
 	}]);
+
+consultaTurnosApp.directive('toupper', function() {
+	return {
+		require: 'ngModel',
+		link: function(scope, element, attrs, modelCtrl) {
+			var mayusculas = function(input) {
+				input ? element.css("text-transform","uppercase") : element.css("text-transform","initial");
+				return input ? input.toUpperCase() : "";
+			};
+
+			modelCtrl.$parsers.push(mayusculas);
+
+			scope.$watch(attrs.ngModel, function(valor){
+				mayusculas(valor);
+			});
+
+			//mayusculas();
+		}
+	};
+});
